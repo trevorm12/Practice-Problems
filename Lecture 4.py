5e2cb247fbd56ebc6b24ef47a510cf4fb5364dec
trevorm12 <ttm@utexas.edu>
2020-09-08 17:05:49 -0500

    Create Lecture 4.py

---
+++ Lecture 4.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import math
+
+#Question 1
+print()
+print("Question 1:")
+print(np.sqrt(23))
+
+#Question 2
+print()
+print("Question 2")
+print(math.sin(math.pi / 2))
+
+#Question 3
+print()
+print("Question 3")
+radius = 2.3
